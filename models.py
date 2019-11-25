7405419f026cec71e15cd935143a81bc9f995aa5
Alexandre Blouin <alexandre.blouin@maplink.global>
2019-11-25 18:56:23 +0100

    Modifications on the model

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fc1_units=512, fc2_units=256):
         """Initialize parameters and build model.
         Params
         ======
@@ -26,6 +26,8 @@ class Actor(nn.Module):
         """
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
+        self.fc1_units = fc1_units
+        self.fc2_units = fc2_units
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
@@ -42,11 +44,15 @@ class Actor(nn.Module):
         x = F.relu(self.fc2(x))
         return F.tanh(self.fc3(x))
 
+    def get_hiddenlayer_size(self):
+        """ Return the size of hidden layers used in the model"""
+        return self.fc1_units, self.fc2_units
+
 
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fcs1_units=512, fc2_units=256):
         """Initialize parameters and build model.
         Params
         ======
@@ -58,6 +64,8 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
+        self.fc1_units = fcs1_units
+        self.fc2_units = fc2_units
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
@@ -74,3 +82,7 @@ class Critic(nn.Module):
         x = torch.cat([xs, action.float()], dim=1)
         x = F.relu(self.fc2(x))
         return self.fc3(x)
+
+    def get_hiddenlayer_size(self):
+        """ Return the size of hidden layers used in the model"""
+        return self.fc1_units, self.fc2_units
